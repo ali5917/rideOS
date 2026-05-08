0ef6d2e2c06e56f4f9a5200afd5bb9b3b570c93e
Ali Kashif <alikashif5917@gmail.com>
2026-05-08 11:50:38 +0500

    add request struct

--- main_server/include/request.h
+++ main_server/include/request.h
@@ -1,7 +1,36 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
-// TODO: Define RideRequest struct and RequestStatus/Type enums.
-// TODO: Declare request_thread and ride_thread functions.
+#include <pthread.h>
+#include <time.h>
+
+typedef enum {
+    NORMAL,
+    EMERGENCY,
+    VIP
+} RequestType;
+
+typedef enum {
+    REQUEST_WAITING,
+    REQUEST_ASSIGNED,
+    REQUEST_COMPLETED,
+    REQUEST_CANCELLED
+} RequestStatus;
+
+typedef struct {
+    int id;
+    RequestType type;           // current type 
+    RequestType originalType;   // assigned type at the time of creation
+    RequestStatus status;
+    int assignedDriverId;
+    float fare;
+    int rideDuration;
+    time_t requestTime;         // time at request creation
+    int timeoutSeconds;         // patience time of request (before it cancels itself)            
+    int deferredCount;          // to prevent starvation
+
+    pthread_mutex_t waitMutex;    // mutex for the request thread to sleep on
+    pthread_cond_t assignedCond;  // signaled by dispatcher when a driver is assigned
+} RideRequest;
 
 #endif
