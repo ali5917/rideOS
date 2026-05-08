7eebfa64abc347ad6ab2a9631966e209020dbdfb
Ismail Silat <ismailsilat7@gmail.com>
2026-05-08 11:39:22 +0500

    Add driver struct

--- main_server/include/driver.h
+++ main_server/include/driver.h
@@ -1,7 +1,29 @@
 #ifndef DRIVER_H
 #define DRIVER_H
 
-// TODO: Define Driver struct and DriverStatus enum.
-// TODO: Implement driver pool management logic.
+#include <time.h>
+#include <stdint.h>
+
+typedef enum {
+    DRIVER_OFFLINE,
+    DRIVER_ONLINE,
+    DRIVER_BUSY,
+    DRIVER_GOING_OFFLINE
+} DriverStatus;
+
+typedef enum {
+    DRIVER_STANDARD, // std cant take vip/emergency ride requests
+    DRIVER_PLUS, // can take vip
+    DRIVER_EMERGENCY // preferred for emergency, can take all
+} DriverCategory;
+
+typedef struct Driver {
+    int ID;
+    DriverCategory category;
+    DriverStatus status;
+    int currentRequestID;
+    time_t lastStatusChange;
+    time_t lastAssignedTime;
+} Driver;
 
 #endif
